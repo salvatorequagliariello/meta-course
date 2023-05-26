5ce51bdcb93e2b48b912be1cc61f9a1a412ab1cd
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-05-26 16:33:56 +0200

    complete first react app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
 
 function Header() {
-  return <h1>Questa è la mia prima App!</h1>
+  return <h1 className="header-title-h1">Questa è la mia prima App!</h1>
 }
 
 function App() {
@@ -11,6 +15,13 @@ function App() {
     <div>
       <Header />
       <Nav />
+      <div className="blog-container">
+        <Intro1 />
+        <Intro2 />
+        <Intro3 />
+      </div>
+      <Footer />
+      
     </div>
   )
 }
--- first-app/src/components/Nav.js
+++ first-app/src/components/Nav.js
@@ -1,8 +1,8 @@
 
-function Nav() {
+function Nav(props) {
     return (
         <nav className="main-nav">
-            <ul>
+            <ul className="nav-elements">
                 <li>Home</li>
                 <li>Articles</li>
                 <li>About</li>
