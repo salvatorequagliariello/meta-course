d3d5920ab5899b0075191e5832a83f46752a17f4
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-05-26 14:26:41 +0200

    complete components part

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function Header() {
   return <h1>Questa è la mia prima App!</h1>
 }
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Nav />
+    </div>
+  )
 }
 
 export default App;
---
+++ first-app/src/components/Intro1.js
@@ -0,0 +1,14 @@
+
+function Intro1() {
+    return (
+        <div className="blog-post-intro">
+            <h2>I've become a react developer!</h2>
+            <div>
+                <p>I've completed the React Basics course and i'm happy to announce that i'm now a Junior React Developer!</p>
+                <p className="link">Read more...</p>
+            </div>
+        </div>
+    );
+};
+
+export default Intro1;
