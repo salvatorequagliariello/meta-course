7d21c864a01edbfe92add8b5cd3ccfd52dc802d1
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-05-30 14:58:17 +0200

    complete meals app

--- meals-provider/src/App.js
+++ meals-provider/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 import MealsProvider from './providers/MealsProvider';
+import MealsList from './components/MealsList';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+        <MealsProvider>
+          <MealsList />
+          <Counter />
+        </MealsProvider>
   );
 }
 
--- meals-provider/src/providers/MealsProvider.js
+++ meals-provider/src/providers/MealsProvider.js
@@ -4,9 +4,9 @@ const MealsContext = createContext();
 
 const todayMeals = ['Baked Beans', 'Beaked Sweet Potatoes', 'Baked Potatoes'];
 
-function MealsProvider(children) {
+function MealsProvider({children}) {
     const [meals, setMealsList] = useState(todayMeals);
-
+    
     return (
         <MealsContext.Provider value={{meals}}>
             {children}
