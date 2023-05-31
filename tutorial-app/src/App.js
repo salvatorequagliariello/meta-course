5bca2ca6bd2536b18d53e09d2a93e137fd3b8089
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-05-31 15:02:31 +0200

    completed app for list

--- tutorial-app/src/App.js
+++ tutorial-app/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount(count + 1);
+
+  return (
+    <div className='counter-button-container'>
+      <button id="counter-button" onClick={handleClick}>
+        Counter = {count}
+      </button>
+    </div>
+  );
+};
+
+function NameList() {
+  const names = ['Jack', 'Jill', 'John'];
+  const [list, setList] = useState(names);
+  const [name, setName] = useState("");
+
+  const handleRemoveClick = () => setList(list.slice(0, -1));
+  const onAddNameButton = () => {
+    setList([...list, name]);
+    setName("");
+  };
+
+  return (
+    <div className='list-container'>
+      <ul>
+        <h2>Add Name</h2>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)}></input>
+        <button onClick={onAddNameButton}>Add</button>
+        {list.map((item) => {
+          return(
+            <li key={item}>{item}</li>
+          )})}
+      </ul>
+      <button onClick={handleRemoveClick}>Remove Name</button>
+    </div>
+  )
+
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter />
+      <NameList />
     </div>
   );
-}
+};
 
 export default App;
