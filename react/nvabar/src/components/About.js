c99d8abe9bc016d7c8d8dcf4b5c63e3bf6d6c08c
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-06-01 15:29:00 +0200

    style new components

--- react/nvabar/src/components/About.js
+++ react/nvabar/src/components/About.js
@@ -1,7 +1,7 @@
 
 function About() {
     return (
-        <div className="about-container">
+        <div className="homepage-container">
             <h1>About</h1>
             <p>
             Duis non viverra metus. Quisque non quam condimentum, feugiat risus at, gravida augue. Curabitur ut congue velit. Ut faucibus euismod sem. In in urna augue. Phasellus pulvinar purus arcu, id aliquet nibh malesuada at. Proin mauris dolor, fringilla tempus diam quis, congue placerat sem. Pellentesque mauris odio, cursus nec arcu sit amet, hendrerit fermentum nibh. Curabitur quis ipsum aliquam, convallis nisl in, dapibus tortor. Aenean gravida dolor sed augue suscipit, nec fringilla velit tempor.
