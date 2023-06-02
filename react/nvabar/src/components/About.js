990bc46700020391774b9b7baef34717f0c69b35
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-06-02 12:37:15 +0200

    style new components

--- react/nvabar/src/components/About.js
+++ react/nvabar/src/components/About.js
@@ -1,11 +1,15 @@
+import AboutImage from  './AboutImage';
 
 function About() {
     return (
         <div className="homepage-container">
             <h1>About</h1>
+            <div className="text-container">
             <p>
             Duis non viverra metus. Quisque non quam condimentum, feugiat risus at, gravida augue. Curabitur ut congue velit. Ut faucibus euismod sem. In in urna augue. Phasellus pulvinar purus arcu, id aliquet nibh malesuada at. Proin mauris dolor, fringilla tempus diam quis, congue placerat sem. Pellentesque mauris odio, cursus nec arcu sit amet, hendrerit fermentum nibh. Curabitur quis ipsum aliquam, convallis nisl in, dapibus tortor. Aenean gravida dolor sed augue suscipit, nec fringilla velit tempor.
             </p>
+            <AboutImage />
+            </div>
         </div>
     );
 };
---
+++ react/nvabar/src/components/AboutImage.js
@@ -0,0 +1,12 @@
+
+function AboutImage() {
+    const abouImage = "https://img.freepik.com/free-photo/diverse-businesspeople-having-meeting_53876-103954.jpg?w=740&t=st=1685700739~exp=1685701339~hmac=070d48add7351e03bbfd44c76e428b7280cd7155a71f6303f2136316f16b0931";
+
+    return (
+        <div className="image-container">
+            <img className="image" src={abouImage}/>
+        </div>
+    );
+};
+
+export default AboutImage;
