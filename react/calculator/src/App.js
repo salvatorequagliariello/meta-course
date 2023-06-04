1bd20460c50972c415093f4f7efb7afffa6434e0
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-06-04 09:24:12 +0200

    complete calculator style

--- react/calculator/src/App.js
+++ react/calculator/src/App.js
@@ -40,34 +40,33 @@ function App() {
  
   return ( 
     <div className="App"> 
-    <div className="calulcator">
-
-      <div className="title-container"> 
-        <h1 className="calculator-title">Calculator</h1> 
-      </div> 
-      <form>
-        <div className="calculator-screen">
-          <input
-            pattern="[0-9]" 
-            ref={inputRef} 
-            type="number" 
-            placeholder="Type a number" 
-            className="calculator-screen"
-            />
-          <p ref={resultRef} className="calculator-result"> 
-            Result: {result} 
-          </p> 
-        </div>
-        <div className="calculator-buttons">
-          <button onClick={plus}>Add</button> 
-          <button onClick={minus}>Subtract</button>
-          <button onClick={times}>Multiply</button>
-          <button onClick={divide}>Divide</button>
-          <button onClick={resetInput}>Cancel</button>
-          <button onClick={resetResult}>Reset</button>
-        </div>
-      </form> 
-            </div>
+      <div className="calculator">
+        <div className="title-container"> 
+          <h1 className="calculator-title">Calculator</h1> 
+        </div> 
+        <form>
+          <div className="calculator-screen">
+            <input
+              pattern="[0-9]" 
+              ref={inputRef} 
+              type="number" 
+              placeholder="Type a number" 
+              className="calculator-screen"
+              />
+            <p ref={resultRef} className="calculator-result"> 
+              Result: {result} 
+            </p> 
+          </div>
+          <div className="calculator-buttons">
+            <button onClick={plus}>Add</button> 
+            <button onClick={minus}>Subtract</button>
+            <button onClick={times}>Multiply</button>
+            <button onClick={divide}>Divide</button>
+            <button onClick={resetInput}>Cancel</button>
+            <button onClick={resetResult}>Reset</button>
+          </div>
+        </form> 
+      </div>
     </div> 
   ); 
 } 
