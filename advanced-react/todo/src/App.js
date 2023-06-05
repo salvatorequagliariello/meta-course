389b3d256a8c3b67d5d892a7b3c9e371b02a7204
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-06-05 12:02:22 +0200

    complete todo list app

--- advanced-react/todo/src/App.js
+++ advanced-react/todo/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDo from './components/TodoList';
+import {useState} from 'react';
 
 function App() {
+  const todosList = [
+    {
+      id: "todo1",
+      createdAt: "18:00"
+    },
+    {
+      id: "todo2",
+      createdAt: "20:30"
+    }
+  ];
+  const [todos, setTodos] = useState(todosList);
+
+  const reverseOrder = () => {
+    setTodos([...todos].reverse());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todos-list'>
+          {todos.map(todo => <ToDo id={todo.id} createdAt={todo.createdAt} />)}
+      </div>
+      <button className='reverse-button' onClick={reverseOrder}>Reverse order</button>
     </div>
   );
 }
