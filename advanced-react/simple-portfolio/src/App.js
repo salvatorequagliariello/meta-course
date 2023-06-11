9113d339511ddbdb96015c227a8099ab40696ea2
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-11 17:59:24 +0200

    init form component and context

--- advanced-react/simple-portfolio/src/App.js
+++ advanced-react/simple-portfolio/src/App.js
@@ -2,18 +2,22 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import Header from './components/Header';
 import LandingSection from './components/LandingSection';
-import ProjectSection from './components/ProjectSection'
-
+import ProjectSection from './components/ProjectSection';
+import ContactMeSection from './components/ContactMeSection';
+import AlertProvider from './context/alertContext'
 
 
 function App() {
   return (
     <ChakraProvider resetCSS="true">
-      <div className="app-container">
-        <Header />
-        <LandingSection />
-        <ProjectSection />
-      </div>
+        <div className="app-container">
+      <AlertProvider>
+          <Header />
+          <LandingSection />
+          <ProjectSection />
+          <ContactMeSection />
+      </AlertProvider>
+        </div>
     </ChakraProvider>
   );
 }
