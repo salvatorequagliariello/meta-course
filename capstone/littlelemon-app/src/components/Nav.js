35ac69b3d6f0edab93b58a9502db97191b069af0
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-13 16:46:16 +0200

    create main components

---
+++ capstone/littlelemon-app/src/components/Nav.js
@@ -0,0 +1,15 @@
+
+function Nav() {
+    return (
+        <nav className="header-nav-section">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#"></a>Menu</li>
+            <li><a href="#"></a>Reservations</li>
+            <li><a href="#"></a>Order Online</li>
+            <li><a href="#"></a>Login</li>
+        </nav>
+    );
+};
+
+export default Nav;
