22b7de20ffb8367ece7ff94f323cdc8a626970de
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-15 17:47:59 +0200

    add variable initialState for the booking times

--- capstone/littlelemon-app/src/App.js
+++ capstone/littlelemon-app/src/App.js
@@ -3,15 +3,21 @@ import Header from "./components/Header";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
 import BookingPage from "./components/BookingPage";
-import { useState, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
+import { fetchAPI, submitAPI } from "./components/fetchdate";
+
+
+
 
 function App() {
   const updateTimes = (state, action) => {
-    return state;
+    
   }
 
-  const initializeTimes = () => {
-    return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const initializeTimes = (day = new Date()) => {
+    const availableTimes = fetchAPI(day);
+
+    return availableTimes;
   }
 
   const [state, dispatch] = useReducer(updateTimes, initializeTimes);
